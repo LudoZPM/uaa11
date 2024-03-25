efbc2550b4eb52cf6b113e2d1300daf6b57ef69b
Ludo <138796711+LudoZPM@users.noreply.github.com>
2024-03-25 14:50:50 +0100

    Add files via upload

---
+++ Ludo_5TTI_Matrice/MethodeDuProjet.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ludo_5TTI_Matrice
+{
+    public struct MethodeMatrices
+    {
+        public int[,] GenMatrice(int lignes, int colonnes)
+        {
+            Random random = new Random();
+            int[,] Matrice = new int[lignes, colonnes];
+
+            for (int i = 0; i < lignes; i++)
+            {
+                for (int j = 0; j < colonnes; j++)
+                {
+                    Matrice[i, j] = random.Next(1, 11);
+                }
+            }
+
+            return Matrice;
+        }
+
+        public void AfficherMatrice(int[,] matrice)
+        {
+            for (int i = 0; i < matrice.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrice.GetLength(1); j++)
+                {
+                    Console.Write(matrice[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void AdditionnerMatrices(int[,] Matrice1, int[,] Matrice2)
+        {
+            int lignes = Matrice1.GetLength(0);
+            int colonnes = Matrice1.GetLength(1);
+
+            int[,] resultat = new int[lignes, colonnes];
+            for (int i = 0; i < lignes; i++)
+            {
+                for (int j = 0; j < colonnes; j++)
+                {
+                    resultat[i, j] = Matrice1[i, j] + Matrice2[i, j];
+                    Console.Write(resultat[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void MultiplierMatrices(int[,] Matrice1, int[,] Matrice2)
+        {
+            int lignes1 = Matrice1.GetLength(0);
+            int colonnes1 = Matrice1.GetLength(1);
+            int lignes2 = Matrice2.GetLength(0);
+            int colonnes2 = Matrice2.GetLength(1);
+
+            if (colonnes1 != lignes2)
+            {
+                Console.WriteLine("Impossible de multiplier les matrices");
+                return;
+            }
+
+            int[,] resultat = new int[lignes1, colonnes2];
+            for (int i = 0; i < lignes1; i++)
+            {
+                for (int j = 0; j < colonnes2; j++)
+                {
+                    for (int k = 0; k < colonnes1; k++)
+                    {
+                        resultat[i, j] += Matrice1[i, k] * Matrice2[k, j];
+                    }
+                    Console.Write(resultat[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
---
+++ Ludo_5TTI_Matrice/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Drawing;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+using System;
+
+namespace Ludo_5TTI_Matrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string nUser; // condition pour l'utilisateur
+            string repeatProg; // condition pour recommencer
+
+            MethodeMatrices matrices = new MethodeMatrices();
+
+            do // boucle recommencer
+            {
+                Console.WriteLine("Bienvenue dans le monde des matrices"); // menu pour matrice
+                Console.WriteLine("Choisissez une opération:\n tapez 1 pour additionner \n tapez 2 pour les multiplier"); // choix de l'opération
+                nUser = Console.ReadLine();
+
+                int lignes; // nombre de lignes 
+                int colonnes; // nombre de colonnes
+
+                Console.ForegroundColor = ConsoleColor.Cyan; // pour mettre le text en cyan
+
+                Console.WriteLine("Entrez le nombre de lignes pour les matrices : ");
+                lignes = int.Parse(Console.ReadLine());
+                Console.WriteLine("Entrez le nombre de colonnes pour les matrices : ");
+                colonnes = int.Parse(Console.ReadLine());
+
+                int[,] Matrice1 = matrices.GenMatrice(lignes, colonnes);
+                int[,] Matrice2 = matrices.GenMatrice(lignes, colonnes);
+
+                if (nUser == "1") // choix numéros 1
+                {
+
+                    Console.ForegroundColor = ConsoleColor.Yellow; // pour mettre le text en jaune
+                    Console.WriteLine("Vous avez choisi l'addition de 2 matrices");
+
+                    matrices.AfficherMatrice(Matrice1); // affiche matrice 1
+                    Console.WriteLine(" + ");
+                    matrices.AfficherMatrice(Matrice2); // affiche matrice 2
+                    Console.WriteLine(" = ");
+                    matrices.AdditionnerMatrices(Matrice1, Matrice2); // affiche matrice additioner
+                    Console.ResetColor(); // retire la couleur
+                }
+                else if (nUser == "2") // choix numéros 2
+                {
+                    Console.ForegroundColor = ConsoleColor.Green; // pour mettre le text en vert
+                    Console.WriteLine("Vous avez choisi la multiplication de 2 matrices");
+                    matrices.AfficherMatrice(Matrice1); // affiche matrice 1
+                    Console.WriteLine(" * ");
+                    matrices.AfficherMatrice(Matrice2); // affiche matrice 2
+                    Console.WriteLine(" = ");
+                    matrices.MultiplierMatrices(Matrice1, Matrice2); // affiche matrice multiplier
+                    Console.ResetColor(); // retire la couleur
+                }
+                else // si un nombre autre que 1 et 2 est choisie 
+                {
+                    Console.ForegroundColor = ConsoleColor.Red; // pour mettre le text rouge
+                    Console.WriteLine("Option invalide. Veuillez choisir une option valide.");
+                    Console.ResetColor(); // retire la couleur
+                }
+
+                Console.WriteLine("Voulez-vous recommencer ? 'o' = oui, 'n' = non"); // condition redémarré
+                repeatProg = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("bonne journée");
+                Console.ResetColor(); // retire la couleur
+
+
+
+            } while (repeatProg == "o"); // boucle while pour recommencer si=o
+        }
+    }
+}
+
+
